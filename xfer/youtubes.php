70a3378896e15ed01c4381eb8ad0062ecda5dd35
Brian Foster <brriann@gmail.com>
2021-03-28 20:17:43 -0600

    Update youtubes.php

--- xfer/youtubes.php
+++ xfer/youtubes.php
@@ -4,16 +4,9 @@ $username = "NOCOMMIT";
 $password = "NOCOMMIT"; 
 $database = "lampdb";
 
+// output server info
 print_r($_SERVER);
 echo "<br>" . "<br>" . "<br>" . "<br>"; 
-if (isset($_GET["url"]) && isset($_GET["name"])) {
-  $url = $_GET["url"];
-  $name = $_GET["name"];
-  echo "url: " . $url . "<br>";
-  echo "name: " . $name . "<br>";
-  echo "<br>" . "<br>" . "<br>"; 
-}
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -21,6 +14,24 @@ $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+}
+
+// handle query parameters
+if (isset($_GET["url"]) && isset($_GET["name"])) {
+  $url = $_GET["url"];
+  $name = $_GET["name"];
+  echo "url: " . $url . "<br>";
+  echo "name: " . $name . "<br>";
+  echo "<br>" . "<br>" . "<br>"; 
+  
+  $fullUrl = "https://www.youtube.com/watch?v=" . $url;
+  
+  $insertSql = "INSERT INTO `youtubes` (`VidName`, `VidUrl`, `DateAdded`) VALUES (" . $name . ", " . $fullUrl . ", NULL)"
+  if ($conn->query($insertSql) === TRUE) {
+    echo "Inserted record from queryString params." . "<br>" . "<br>";
+  } else {
+      echo "Error inserting record from queryString" . "<br>" . "<br>";
+  }
 }
 
 $sql = "SELECT Id, VidName, VidUrl FROM youtubes";
