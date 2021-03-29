ebce2bbcb3f73e31879d8377706ad4076c18725c
Brian Foster <brriann@gmail.com>
2021-03-28 20:35:37 -0600

    Update youtubes.php

--- xfer/youtubes.php
+++ xfer/youtubes.php
@@ -16,23 +16,25 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// // handle query parameters
-// if (isset($_GET["url"]) && isset($_GET["name"])) {
-//   $url = $_GET["url"];
-//   $name = $_GET["name"];
-//   echo "url: " . $url . "<br>";
-//   echo "name: " . $name . "<br>";
-//   echo "<br>" . "<br>" . "<br>"; 
+// handle query parameters
+if (isset($_GET["url"]) && isset($_GET["name"])) {
+  $url = $_GET["url"];
+  $name = $_GET["name"];
+  echo "url: " . $url . "<br>";
+  echo "name: " . $name . "<br>";
+  echo "<br>" . "<br>" . "<br>"; 
   
-//   $fullUrl = "https://www.youtube.com/watch?v=" . $url;
+  $fullUrl = "https://www.youtube.com/watch?v=" . $url;
+  echo "fullUrl: " . $fullUrl . "<br>";
   
-//   $insertSql = "INSERT INTO `youtubes` (`VidName`, `VidUrl`, `DateAdded`) VALUES ('" . $name . "', '" . $fullUrl . "', NULL)"
-//   if ($conn->query($insertSql) === TRUE) {
-//     echo "Inserted record from queryString params." . "<br>" . "<br>";
-//   } else {
-//       echo "Error inserting record from queryString" . "<br>" . "<br>";
-//   }
-// }
+  $insertSql = "INSERT INTO `youtubes` (`VidName`, `VidUrl`, `DateAdded`) VALUES ('" . $name . "', '" . $fullUrl . "', NULL)";
+  echo "insertSql: " . $insertSql . "<br>";
+  if ($conn->query($insertSql) === TRUE) {
+    echo "Inserted record from queryString params." . "<br>" . "<br>";
+  } else {
+      echo "Error inserting record from queryString" . "<br>" . "<br>";
+  }
+}
 
 $sql = "SELECT Id, VidName, VidUrl FROM youtubes";
 $result = $conn->query($sql);
